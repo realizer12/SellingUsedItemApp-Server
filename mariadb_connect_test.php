0603f9d48a7dfbb2c18fcf7524b69a2d8ca659ad
LeeDonghun <dlwldlwldus123@gmail.com>
2020-06-27 23:27:32 +0900

    add sms auth code on server and  put them on the  gitignore file to hide private info

--- mariadb_connect_test.php
+++ mariadb_connect_test.php
@@ -4,19 +4,24 @@
 include('DbConnection/dbcon.php');
 
 $name="shawn";
-$select_data_stmt = $pdo->prepare('SELECT * FROM test_table WHERE name= :name');
+$select_data_stmt = $pdo->prepare('SELECT * FROM test_table');
 $select_data_stmt->bindValue(':name',$name,PDO::PARAM_STR);
 $select_data_stmt->execute();
 $result = $select_data_stmt->fetch(PDO::FETCH_ASSOC);
-   
-print_r($result);
 
+
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";
 
 // while (($result = $select_data_stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
     
 //     echo 'db에서 가지고 온 값 ->  '.$result['name'].'</br>';
 // }
 
+
+$conn
 
 
 ?>
