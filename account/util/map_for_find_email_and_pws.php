8991290c5d8fb7c0203e6f6f77ccba57ec23b1bd
LeeDonghun <dlwldlwldus123@gmail.com>
2020-08-31 01:12:19 +0900

    핸드폰 인증 기능 아이디 찾기 부분 수정 및  비밀번호 찾기용 코드  추가

--- account/util/map_for_find_email_and_pws.php
+++ account/util/map_for_find_email_and_pws.php
@@ -23,8 +23,21 @@ $login_email=$_POST['login_email'];
 //휴대폰 인증 하는 이유  0=회원가입, 1=아이디 찾기, 2= 비밀 번호 찾기
 $map_reason=$_POST['map_reason'];
 
-//핸드폰 번호 중복 되는지 확인하기
-$check_phone_number_used=$pdo->prepare('SELECT phone_num FROM member_info WHERE phone_num=:phone_number');
+
+if($map_reason==1){//아이디 찾기
+
+    //핸드폰 번호 중복 되는지 확인하기
+    $check_phone_number_used=$pdo->prepare('SELECT phone_num FROM member_info WHERE phone_num=:phone_number');
+
+}else if($map_reason==2){//비밀번호 찾기일때
+
+     //핸드폰 번호 중복 되는지 확인하기
+     $check_phone_number_used=$pdo->prepare('SELECT * FROM member_info WHERE phone_num=:phone_number AND email=:login_email');
+    //핸드폰 번호  데이터 바인딩
+    $check_phone_number_used->bindValue('login_email',$login_email);
+    
+}
+
 
 //핸드폰 번호  데이터 바인딩
 $check_phone_number_used->bindValue('phone_number',$sended_phone_number);
@@ -37,50 +50,109 @@ $check_phone_number_used->execute();
 
 if($check_phone_number_used){
 
-    $result=$check_phone_number_used->fetchColumn();
 
-   if($result!=null){//중복값 있음
+    if($map_reason==1){//아이디 찾기일때
+
+        $result=$check_phone_number_used->fetchColumn();
+
+        if($result!=null){//중복값 있음
+            
+    
+            //prepare 문을 사용해서 member_auth_phone_num 테이블 insert 쿼리 준비
+            $insert_phone_auth_data_stmt=$pdo->prepare(
+                'INSERT INTO member_auth_phone_num 
+                (map_phone_number,map_key,map_reason) 
+                values(:phone_num,:map_key,:map_reason)');
+
+            //5자리 랜덤 key 생성
+            $map_key_number = sprintf('%05d',rand(00000,99999));
+
+            $insert_phone_auth_data_stmt->bindValue('phone_num',$sended_phone_number);//폰번호 바인딩
+            $insert_phone_auth_data_stmt->bindValue('map_key',$map_key_number);//인증 키 바인딩
+            $insert_phone_auth_data_stmt->bindValue('map_reason',$map_reason);//인증 출처 바인딩
+
+            try{
+
+                //쿼리  실행
+                $insert_phone_auth_data_stmt->execute();
+
+                //insert 성공시 앱으로 성공메세지 1 보냄
+                if($insert_phone_auth_data_stmt){
+            
+                    include('../../sms_auth/sendsms.php');//청기와랩 api sms  보내는 파일
+                    echo "1";//중복값 없어서  sms 인증코드 보냄
+
+                }
+
+            }catch(PDOException $e){
+
+                echo "$e";
+            }
+                        
+
+        }else{//중복값없음
+    
+        echo "5";//중복값 없음
         
+        }// db    중복 번호가 없을 경우 끝
 
-        //prepare 문을 사용해서 member_auth_phone_num 테이블 insert 쿼리 준비
-        $insert_phone_auth_data_stmt=$pdo->prepare(
-            'INSERT INTO member_auth_phone_num 
-            (map_phone_number,map_key,map_reason) 
-            values(:phone_num,:map_key,:map_reason)');
 
-        //5자리 랜덤 key 생성
-        $map_key_number = sprintf('%05d',rand(00000,99999));
 
-        $insert_phone_auth_data_stmt->bindValue('phone_num',$sended_phone_number);//폰번호 바인딩
-        $insert_phone_auth_data_stmt->bindValue('map_key',$map_key_number);//인증 키 바인딩
-        $insert_phone_auth_data_stmt->bindValue('map_reason',$map_reason);//인증 출처 바인딩
+    }else if($map_reason==2){//비밀번호 찾기일때
 
-        try{
-
-            //쿼리  실행
-            $insert_phone_auth_data_stmt->execute();
-
-            //insert 성공시 앱으로 성공메세지 1 보냄
-            if($insert_phone_auth_data_stmt){
+            $result=$check_phone_number_used->fetch();
         
-                include('../../sms_auth/sendsms.php');//청기와랩 api sms  보내는 파일
-                echo "1";//중복값 없어서  sms 인증코드 보냄
+            if($result!=null){//중복값 있음
+
+
+                if($result['sns_check']>0){//sns 아이디일때
+
+                    echo "2";
+
+                }else{//일반 로그인 이메일 일때
+
+                    //prepare 문을 사용해서 member_auth_phone_num 테이블 insert 쿼리 준비
+                    $insert_phone_auth_data_stmt=$pdo->prepare(
+                        'INSERT INTO member_auth_phone_num 
+                        (map_phone_number,map_key,map_reason) 
+                        values(:phone_num,:map_key,:map_reason)');
+
+                    //5자리 랜덤 key 생성
+                    $map_key_number = sprintf('%05d',rand(00000,99999));
+
+                    $insert_phone_auth_data_stmt->bindValue('phone_num',$sended_phone_number);//폰번호 바인딩
+                    $insert_phone_auth_data_stmt->bindValue('map_key',$map_key_number);//인증 키 바인딩
+                    $insert_phone_auth_data_stmt->bindValue('map_reason',$map_reason);//인증 출처 바인딩
+
+                    try{
+
+                        //쿼리  실행
+                        $insert_phone_auth_data_stmt->execute();
+
+                        //insert 성공시 앱으로 성공메세지 1 보냄
+                        if($insert_phone_auth_data_stmt){
+                    
+                            include('../../sms_auth/sendsms.php');//청기와랩 api sms  보내는 파일
+                            echo "1";//중복값 없어서  sms 인증코드 보냄
+
+                        }
+
+                    }catch(PDOException $e){
+
+                        echo "$e";
+                    }
+                }
+
+
+            }else{//중복값 없음
+
+                echo "5";
 
             }
 
-        }catch(PDOException $e){
+    }
 
-            echo "$e";
-        }
-
-
-    
-
-   }else{//중복값없음
-
-    echo "5";//중복값 없음
-     
-   }// db    중복 번호가 없을 경우 끝
+   
 
 }else{//핸드폰 중복 여부 조회  실패 
 
@@ -92,16 +164,6 @@ if($check_phone_number_used){
 
     echo "$e";
 }
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
